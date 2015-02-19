f2b87bd72583dcc4c06f215122e099a5d75852ae
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-02-19 14:03:54 -0800

    added apple, removed hardcoding

--- Documents/GitHub/PyGames/snake.py
+++ Documents/GitHub/PyGames/snake.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
 
 pygame.init()
 
@@ -13,17 +15,89 @@ display_height = 600
 # print(x)r
 # returns tuple with passes and fails
 
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Simple Snake')
 
 # pygame.display.update()
 
-gameExit = False
 
-lead_x = 00
-lead_y = 300
-lead_x_change = 0
-lead_y_change = 0
+clock = pygame.time.Clock()
+block_size = 10
+FPS = 30
+
+font = pygame.font.SysFont(None, 25)
+
+def message_to_screen(msg,color):
+    screen_text = font.render(msg, True, color)
+    gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+
+    
+def gameLoop():
+    gameExit = False
+    gameOver = False
+
+    lead_x = display_width/2
+    lead_y = display_height/2
+    lead_x_change = 0
+    lead_y_change = 0
+
+    randAppleX = random.randrange(0, display_width-block_size)
+    randAppleY = random.randrange(0, display_height-block_size)
+    
+    while not gameExit:
+
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("game over! C = play again Q = quit", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_y_change = -block_size
+                    lead_x_change = 0
+                elif event.key == pygame.K_DOWN:
+                    lead_y_change = block_size
+                    lead_x_change = 0
+
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+            gameOver = True
+
+
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, block_size, block_size])
+        pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
+        # () = where,color,coordinates and size
+        pygame.display.update()
+
+        clock.tick(FPS)
+                    
+#    message_to_screen("You Loose", red)
+#    pygame.display.update()
+#    time.sleep(2)
+    pygame.quit()
+    quit()
+
+gameLoop()
 
 clock = pygame.time.Clock()
 
