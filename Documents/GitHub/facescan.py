878bd5e1e9c87673e89a455f7308a7b2913fe58d
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-02-22 23:24:18 -0800

    Update facescan.py

--- Documents/GitHub/facescan.py
+++ Documents/GitHub/facescan.py
@@ -6,3 +6,5 @@ from pywinauto.application import Application as app
 fsv = app.Start("mshello.exe")
 
 fsv.InstallDialog.NextButton.Wait('ready', timeout=30).ClickInput()
+fsv.InstallDialog.Wait('ready', timeout=30).TypeKeys(os.getcwd() + "\FastStone Image Viewer", with_spaces=True)
+fsv.InstallDialog.End.Wait('ready', timeout=30).ClickInput()
