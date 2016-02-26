f35d185f6deba40ed6059e3b8535b967adc72119
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-02-25 16:03:22 -0800

    Update facescan.py

--- Documents/GitHub/facescan.py
+++ Documents/GitHub/facescan.py
@@ -8,3 +8,5 @@ fsv = app.Start("mshello.exe")
 fsv.InstallDialog.NextButton.Wait('ready', timeout=30).ClickInput()
 fsv.InstallDialog.Wait('ready', timeout=30).TypeKeys(os.getcwd() + "\FastStone Image Viewer", with_spaces=True)
 fsv.InstallDialog.End.Wait('ready', timeout=30).ClickInput()
+
+fsv.InstallDialog.FinishButton.Wait('ready', timeout=30).ClickInput()
