7254e927e8680e0cf4b93dd4f5e2de12694d3e6c
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-05-19 21:16:05 -0700

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -3,3 +3,12 @@ import socket
 import subprocess
 import sys
 from datetime import datetime
+
+subprocess.call('clear', shell=True)
+
+remoteServer    = raw_input("Enter a remote host to scan: ")
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+print "-" * 60
+print "scanning remote host", remoteServerIP
+print "-" * 60
