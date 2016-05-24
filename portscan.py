a61b778a4d23249748c0c875e3281d459f2ce195
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-05-23 22:32:14 -0700

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -12,3 +12,31 @@ remoteServerIP  = socket.gethostbyname(remoteServer)
 print "-" * 60
 print "scanning remote host", remoteServerIP
 print "-" * 60
+
+1 = datetime.now()
+
+try:
+    for port in range(1,1025):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print "Port {}: 	 Open".format(port)
+        sock.close()
+
+except KeyboardInterrupt:
+    print "You pressed Ctrl+C"
+    sys.exit()
+
+except socket.gaierror:
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+except socket.error:
+    print "Couldn't connect to server"
+    sys.exit()
+
+t2 = datetime.now()
+
+total =  t2 - t1
+
+print 'Finished in: ', total
