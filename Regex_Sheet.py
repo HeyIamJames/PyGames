7188258ce016cc069ec6a4c151f60688b0ec9d5d
James Hemmaplardh <jameshemmaplardh@gmail.com>
2016-12-01 12:49:02 -0800

    Update Regex_Sheet.py

--- Regex_Sheet.py
+++ Regex_Sheet.py
@@ -5,7 +5,16 @@
 import re
 re.findall(r"\D(\d{6})\D", s)
 
+#include 6+
+only len 6 = /([\W\w])\d{6}
+#only 6
+^\D*\d{15}\D*$
 
 #how to replace char
 
 re.sub('[d]', ' ', x)
+
+
+>>> str = "h3110 23 cat 444.4 rabbit 11 2 dog"
+>>> [int(s) for s in str.split() if s.isdigit()]
+[23, 11, 2]
